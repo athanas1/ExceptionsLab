da2ac7f5dd6aaa5d075c3211ab82065d0055faf3
athanas1 <athanas1@L11404043631.wctc.msft>
2016-03-10 14:04:09 -0600

    Exceptions Lab #2 finishing up

--- src/lab2/InputOutputGui.java
+++ src/lab2/InputOutputGui.java
@@ -4,26 +4,34 @@ import javax.swing.JOptionPane;
 
 /**
  * This class is responsible for all input and output in the program.
- * 
- * @author  Jim Lombardo, jlombardo@wctc.edu
+ *
+ * @author Jim Lombardo, jlombardo@wctc.edu
  * @version 1.00
  */
 public class InputOutputGui {
+
     private NameService nameService;
 
     public InputOutputGui() {
         nameService = new NameService();
     }
 
-    public void startConversation() {
-        
-        String fullName = JOptionPane.showInputDialog("Enter full name:");
+    public void startConversation() throws IllegalArgumentException {
+
         String lastName = "";
-        lastName = nameService.extractLastName(fullName);
-  
-        String msg = "Your last name is: " + lastName;
+
+        while (lastName.isEmpty()) {
+            String fullName = JOptionPane.showInputDialog("Enter full name:");
+            try {
+                lastName = nameService.extractLastName(fullName);
+
+            } catch (IllegalArgumentException e) {
+                JOptionPane.showMessageDialog(null, "Please enter a correct name format");
+            }
+        }
+        String msg = "Your Last name is:" + lastName;
         JOptionPane.showMessageDialog(null, msg);
-        
+
     }
-     
+
 }
--- src/lab2/NameService.java
+++ src/lab2/NameService.java
@@ -1,33 +1,34 @@
 package lab2;
 
 /**
- * This class provides various services relating to name manipulation.
- * No output should be performed here.
- * 
- * @author  Jim Lombardo, jlombardo@wctc.edu
+ * This class provides various services relating to name manipulation. No output
+ * should be performed here.
+ *
+ * @author Jim Lombardo, jlombardo@wctc.edu
  * @version 1.00
  */
 public class NameService {
-    
+
     /**
-     * Finds and returns the last name from within a full name. Caution: 
-     * No validation is performed.
-     * 
+     * Finds and returns the last name from within a full name. Caution: No
+     * validation is performed.
+     *
      * @param fullName - a name containing a first name and a last name
      * @return the last name
-     * @throws IllegalArgumentException if fullName is null or empty or has 
+     * @throws IllegalArgumentException if fullName is null or empty or has
      * fewer than two parts
      */
-    public String extractLastName(String fullName) {
-        String lastName = null;
-        
-        // write your code here to extract the lastName and store in the
-        // above local variable
-        
-        return lastName;
-    }
-    
+    public String extractLastName(String fullName) throws IllegalArgumentException {
 
-    
-    
+        if (fullName == null || fullName.isEmpty()) {
+            throw new IllegalArgumentException("Your Full name cannot be empty");
+        }
+        String[] name = fullName.split(" ");
+        if (name.length > 2 || name.length < 2) {
+            throw new IllegalArgumentException("Incorrect Format");
+        }
+
+        return name[name.length - 1];
+    }
+
 }
